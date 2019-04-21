a11e6ed70fd259ac80b2483abed162e46abda9ed
junchen <junchen1314@foxmail.com>
2019-04-21 18:33:28 +0800

    项目初始化

---
+++ netty-proxy-client/src/main/java/tianhui/xin/cloud/proxy/client/ProxyClientApplication.java
@@ -0,0 +1,11 @@
+package tianhui.xin.cloud.proxy.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyClientApplication {
+    public static void main(String [] args){
+        SpringApplication.run(ProxyClientApplication.class,args);
+    }
+}
---
+++ netty-proxy-client/src/main/java/tianhui/xin/cloud/proxy/client/web/IndexController.java
@@ -0,0 +1,13 @@
+package tianhui.xin.cloud.proxy.client.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class IndexController {
+    @GetMapping
+    public Mono<String> index(){
+        return Mono.create(r  -> r.success("hello world!"));
+    }
+}
